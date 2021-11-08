e699840ec9015e4bca24c1a5690d32f3f942c597
shi <shi@crtweb.ru>
2021-11-08 11:51:38 +0500

    docker config

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService, Movie } from "./app.service";
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/movies')
+  getMovies(): Movie[] {
+    return this.appService.getMovies();
+  }
+}
